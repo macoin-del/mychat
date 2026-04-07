d44419ecfaa30647973e50405daaf9d902974f90
macoin-del <normalinus3@gmail.com>
2026-04-07 15:16:12 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,31 @@ const io = require("socket.io")(http);
 
 app.use(express.static("public"));
 
+const ADMIN_NAME = "admin";
+
 io.on("connection", (socket) => {
+    let userName = "user";
+
+    socket.on("set name", (name) => {
+        userName = name;
+    });
+
     socket.on("chat message", (msg) => {
-        io.emit("chat message", msg);
+        io.emit("chat message", {
+            user: userName,
+            text: msg
+        });
+    });
+
+    socket.on("admin msg", (data) => {
+        if (userName === ADMIN_NAME) {
+            io.emit("chat message", {
+                user: "👑 ADMIN",
+                text: data
+            });
+        }
     });
 });
 
-http.listen(3000, () => {
-    console.log("running");
-});
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log("running"));
